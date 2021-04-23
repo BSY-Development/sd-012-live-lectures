5a7bbeb9eab677ae4f845ebb153e304195f4c801
Gustavo Caetano <gustavo.caetano@betrybe.com>
2021-04-23 18:02:50 -0300

    refatora o código pra ficar muito mais bonito

--- megasena.js
+++ megasena.js
@@ -10,17 +10,24 @@ let sixthNumber = Math.ceil(Math.random() * 60);
 megaSenaNumbers = [firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber];
 
 let turma12Play = [35, 8, 22, 14, 2, 60];
+let turma12Play2 = [1, 2, 3, 4, 5, 6];
+let turma12Play3 = [7, 8, 9, 10, 11, 12];
+let turma12Play4 = [17, 18, 19, 20, 21, 22];
 
-let numberOfHits = 0;
+let megaSenaGames = [turma12Play, turma12Play2, turma12Play3, turma12Play4];
 
-for (let indexMega = 0; indexMega < megaSenaNumbers.length; indexMega += 1) {
-  for (let indexPlay = 0; indexPlay < turma12Play.length; indexPlay += 1) {
-    if (megaSenaNumbers[indexMega] === turma12Play[indexPlay]) {
-      numberOfHits += 1;
+for (let games = 0; games < megaSenaGames.length; games += 1) {
+  let numberOfHits = 0;
+  for (let indexMega = 0; indexMega < megaSenaNumbers.length; indexMega += 1) {
+    for (let indexPlay = 0; indexPlay < megaSenaGames[games].length; indexPlay += 1) {
+      if (megaSenaNumbers[indexMega] === megaSenaGames[games][indexPlay]) {
+        numberOfHits += 1;
+      };
     };
   };
+  console.log('Jogada ' + (games + 1));
+  console.log(`Mega-Sena: ${megaSenaNumbers}`);
+  console.log(`Jogo: ${megaSenaGames[games]}`);
+  console.log(`Acertos: ${numberOfHits}`);
+  console.log('--------------------------');
 };
-
-console.log(megaSenaNumbers);
-console.log(turma12Play);
-console.log(numberOfHits);
