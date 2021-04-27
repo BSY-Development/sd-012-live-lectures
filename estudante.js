c9c7aefbd17a1d92c6c4ec21e23b3f56a5c993c0
Gustavo Caetano <gustavo.caetano@betrybe.com>
2021-04-27 16:59:20 -0300

    adiciona estudante.js e refatorando.js

---
+++ estudante.js
@@ -0,0 +1,54 @@
+let estudante = {
+  nome: 'Joana',
+  idade: 43,
+  hardSkills: 3,
+  softSkills: 6,
+  carreiraSkills: 5
+};
+
+estudante.cidade = 'São Paulo';
+estudante['comidaFavorita'] = 'Doritos';
+estudante['nome'] = 'João';
+estudante.apelidos = ['apelido1', 'apelido2', 'apelido3'];
+estudante.endereco = {
+  lagradouro: 'Rua Dalvo Trombeta',
+  numero: 357
+}
+
+for (let key in estudante) {
+  console.log(key, estudante[key]);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------------------------------//
+//FUNÇÕES
+
+function trybe(estudanteTrybe) {
+  let pessoaDesenvolvedora = estudanteTrybe;
+
+  pessoaDesenvolvedora.hardSkills = 10;
+  pessoaDesenvolvedora.softSkills = 10;
+  pessoaDesenvolvedora.carreiraSkills = 10;
+  pessoaDesenvolvedora.tryber = true;
+
+  return pessoaDesenvolvedora;
+};
