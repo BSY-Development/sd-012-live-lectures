51de538cfb8108db19e750c686ea3c27bcef3baf
Gustavo Caetano <gustavo.caetano@betrybe.com>
2021-05-03 17:59:46 -0300

    carrinhos bonitos e alegres :D

--- script.js
+++ script.js
@@ -11,5 +11,20 @@ window.onload = function () {
   button.addEventListener('click', function () {
     car1.style.marginLeft = parseInt(car1.style.marginLeft) + (Math.random() * 100) + 'px';
     car2.style.marginLeft = parseInt(car2.style.marginLeft) + (Math.random() * 100) + 'px';
+
+    if (parseInt(car1.style.marginLeft) > window.innerWidth) {
+      alert('Carro VERMELHO ganhou!!');
+      resetCars(car1, car2);
+    };
+
+    if (parseInt(car2.style.marginLeft) > window.innerWidth) {
+      alert('Carro VERDE ganhou!!');
+      resetCars(car1, car2);
+    };
   });
+};
+
+function resetCars(car1, car2) {
+  car1.style.marginLeft = '0px';
+  car2.style.marginLeft = '0px';
 };
